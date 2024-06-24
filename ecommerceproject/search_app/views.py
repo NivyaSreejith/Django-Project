5c08a5fafb847cde0b38b99552afbad4ce3224b1
NivyaSreejith <131278968+NivyaSreejith@users.noreply.github.com>
2024-06-24 15:22:46 +0400

    Update views.py

--- ecommerceproject/search_app/views.py
+++ ecommerceproject/search_app/views.py
@@ -2,12 +2,9 @@ from django.shortcuts import render
 from shopapp.models import Product
 from django.db.models import Q
 
-# Create your views here.
-
 def SearchResult(request):
-    products=None
-    query=None
-    if 'q' in request.GET:
-        query=request.GET.get('q')
-        products=Product.objects.all().filter(Q(name__contains=query) | Q(description__contains=query))
-        return render(request,'search.html',{'query':query,'products':products})
+    products = None
+    query = request.GET.get('q')
+    if query:
+        products = Product.objects.filter(Q(name__icontains=query) | Q(description__icontains=query))
+    return render(request, 'search.html', {'query': query, 'products': products})
